35cd38b45ff83c115b972c477f2d2d71257d66b2
chaeeun <kimce0405@soongsil.ac.kr>
2023-02-14 17:58:05 +0900

    2.14

--- Pt 3/challenge6.c
+++ Pt 3/challenge6.c
@@ -1,38 +1,50 @@
+//야구게임. 같은 숫자가 두개나올시에 2ball이 되는 오류 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(){
     srand((int)time(NULL));
-    int randNum[3],myNum[3];
-    int j,m,n,try = 1;
+    int randNum[3];
+    int try = 1;
     for(int i = 0; i < 3; i++){
-        randNum[i] = rand()%10;
+        randNum[i] = rand()%10; //randNum 배열에 랜덤 숫자 0-9 3개 
     }
+    for(int i = 0; i < 3; i++){
+        printf("%d ",randNum[i]); //randNum check
+    }
+
     printf("Start Game!\n");
+
     while(1){
+        int myNum[3];
         int strike = 0, ball = 0;
         printf("3개의 숫자 선택 : ");
+
         for(int i = 0; i < 3; i++){
             scanf("%d",&myNum[i]);
-        }
+        } // 숫자 세개 mynum배열에 저장
+
         printf("%d 번째 도전 결과 : ",try);
         for(int i = 0; i < 3; i++){
             if(randNum[i] == myNum[i])
                 strike++;
-        }
+        } //위치까지 같으면 strike 
+
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                if(randNum[i] == randNum[j])
+                if(randNum[i] == myNum[j])
                 ball++;
             }
-        }
-        printf("%dstrike, %dball!!\n",strike,ball-strike);
+        } //ball 하나하나씩 확인하면서 같은게 있으면 ball
+        printf("%dstrike, %dball!!\n",strike,ball-strike); //ball에서 strike 빼기
+
         if(strike == 3)
             break;
-        try++;
 
-        
+        try++;
+  
     }
+    printf("\n\nGame Over!\n");
     
 }
